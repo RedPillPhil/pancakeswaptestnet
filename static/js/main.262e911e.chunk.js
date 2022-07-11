3b40221b9c44bda4908b57af72cf0a7a5e8f1e37
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-10 21:59:15 -0400

    Update main.262e911e.chunk.js

--- static/js/main.262e911e.chunk.js
+++ static/js/main.262e911e.chunk.js
@@ -311,7 +311,7 @@
         G = n(27),
         X = n(13),
         J = n(6),
-        $ = "0x715561329f1216F79037feBbf107d951d87AcDe6",
+        $ = "0x627472ac3B206D42DC5DF21A5c35D8F714435d90",
         Z = J.a.BSCTESTNET,
         ee = new J.j(Z, "0x8a9424745056Eb399FD19a0EC26A14316684e274", 18, "DAI", "Dai Stablecoin"),
         te = new J.j(Z, "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", 18, "BUSD", "Binance USD"),
@@ -4029,7 +4029,7 @@
           console.info('Failed to parse input amount: "'.concat(e, '"'), r)
         }
       }
-      var Es = ["0xb7926c0430afb07aa7defde6da862ae0bde767bc", "0x715561329f1216F79037feBbf107d951d87AcDe6", "0x715561329f1216F79037feBbf107d951d87AcDe6"];
+      var Es = ["0xb7926c0430afb07aa7defde6da862ae0bde767bc", "0x627472ac3B206D42DC5DF21A5c35D8F714435d90", "0x627472ac3B206D42DC5DF21A5c35D8F714435d90"];
 
       function Ns(e, t) {
         return e.route.path.some((function(e) {
