3a1a7d47fcd1ed36a63b2591608c98933231537a
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-13 04:49:52 -0400

    Update main.262e911e.chunk.js

--- static/js/main.262e911e.chunk.js
+++ static/js/main.262e911e.chunk.js
@@ -292,10 +292,10 @@
         urls: Object(C.a)({}, z, Y)
       });
       var V, W = new k.a({
-          supportedChainIds: [56]
+          supportedChainIds: [97]
         }),
         H = new E.BscConnector({
-          supportedChainIds: [56]
+          supportedChainIds: [97]
         }),
         Q = new T.b({
           rpc: Object(C.a)({}, z, Y),
@@ -311,7 +311,7 @@
         G = n(27),
         X = n(13),
         J = n(6),
-        $ = "0x86ad49A1d665087C6Cce9D5874b8Bc2a23e0F8A0",
+        $ = "0xa4208bBe230c99685ba3985203061B73F049637A",
         Z = J.a.BSCTESTNET,
         ee = new J.j(Z, "0x8a9424745056Eb399FD19a0EC26A14316684e274", 18, "DAI", "Dai Stablecoin"),
         te = new J.j(Z, "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", 18, "BUSD", "Binance USD"),
@@ -4029,7 +4029,7 @@
           console.info('Failed to parse input amount: "'.concat(e, '"'), r)
         }
       }
-      var Es = ["0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", "0x86ad49A1d665087C6Cce9D5874b8Bc2a23e0F8A0", "0x86ad49A1d665087C6Cce9D5874b8Bc2a23e0F8A0"];
+      var Es = ["0xb7926c0430afb07aa7defde6da862ae0bde767bc", "0xa4208bBe230c99685ba3985203061B73F049637A", "0xa4208bBe230c99685ba3985203061B73F049637A"];
 
       function Ns(e, t) {
         return e.route.path.some((function(e) {
