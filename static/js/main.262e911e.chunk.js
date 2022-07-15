39b435f0e74003d97bdb12644a29340759f86ad2
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-07-15 17:55:09 -0400

    Update main.262e911e.chunk.js

--- static/js/main.262e911e.chunk.js
+++ static/js/main.262e911e.chunk.js
@@ -311,7 +311,7 @@
         G = n(27),
         X = n(13),
         J = n(6),
-        $ = "0x18513CCB2843626a6A13dF30D7C5E8E42F1dA97e",
+        $ = "0x0d500562dF38cb996c1a2D33E67a4D0Fc48E01d6",
         Z = J.a.BSCTESTNET,
         ee = new J.j(Z, "0x8a9424745056Eb399FD19a0EC26A14316684e274", 18, "DAI", "Dai Stablecoin"),
         te = new J.j(Z, "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", 18, "BUSD", "Binance USD"),
